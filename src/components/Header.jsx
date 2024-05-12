a62121fc24da00631a5992a93acc848e0b5e2e04
Robert Walker <robertWalker6580@gmail.com>
2024-05-12 11:51:10 -0700

    create site header

---
+++ src/components/Header.jsx
@@ -0,0 +1,37 @@
+import MenuItem from '@/components/MenuItem'
+import { AiFillHome } from 'react-icons/ai'
+import { BsFillInfoCircleFill } from 'react-icons/bs'
+import Link from 'next/link'
+
+const Header = () => {
+  return (
+    <header>
+      <div className='flex justify-between items-center p-3 max-w-6xl mx-auto'>
+        <div className='flex gap-4'>
+          <MenuItem
+            title='home'
+            url='/'
+            Icon={AiFillHome}
+          />
+          <MenuItem
+            title='about'
+            url='/about'
+            Icon={BsFillInfoCircleFill}
+          />
+        </div>
+
+        <Link
+          href='/'
+          className='flex gap-1 items-center'
+        >
+          <span className='text-2xl font-bold bg-amber-500 py-1 px-2 rounded-lg'>
+            IMDb
+          </span>
+          <span className='text-xl hidden sm:inline'>Clone</span>
+        </Link>
+      </div>
+    </header>
+  )
+}
+
+export default Header
---
+++ src/components/MenuItem.jsx
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+
+const MenuItem = ({ title, url, Icon }) => {
+  return (
+    <Link
+      href={url}
+      className='hover:text-amber-500'
+    >
+      <Icon className='text-2xl sm:hidden' />
+      <p className='uppercase hidden sm:inline text-sm'>{title}</p>
+    </Link>
+  )
+}
+
+export default MenuItem
